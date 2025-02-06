8973696dbe627f17e2ba182fe883548f8a8e1ad4
Anas Ahammad <anasahammad2002@gmail.com>
2025-02-06 20:30:59 +0600

    Add optional image field to brand model

--- models/brand.model.js
+++ models/brand.model.js
@@ -3,6 +3,7 @@ const {  mongoose } = require("mongoose");
 
 const brandSchema = new mongoose.Schema({
     name: { type: String, required: true },
+    image: {type: String, required: false},
     description: { type: String, required: false },
     isActive: { type: Boolean, default: true },
     createdAt: { type: Date, default: Date.now }
